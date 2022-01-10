8e39245d206c9515c56065e5995a4e18063b35ce
Carolina Musso <42645384+cmusso86@users.noreply.github.com>
2022-01-10 18:07:43 -0300

    Delete token.R
    salvei no meu computador só...

--- token.R
+++
@@ -1,9 +0,0 @@
-usethis::edit_r_environ()
-
-credentials::set_github_pat("GITHUB_PAT_trad_cesar")
-
-#2021
-#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
-
-# Jan 10, 2022
-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
